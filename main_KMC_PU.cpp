3f685f5bc12380766af8834197381dbfa438c1ee
Matthew Coile <mcoile@u.northwestern.edu>
2020-06-15 14:30:04 -0500

    version 2, still buggy  probably due to chain-chain adddition and frontend/backend counting mistakes

--- main_KMC_PU.cpp
+++ main_KMC_PU.cpp
@@ -28,17 +28,18 @@ int main() {
     // need to randomly select whether A-B addition or B-A addition occurs.... then need to randomly select the monomer and the side of the monomer that is added to..
     int moleculesA = 1000; // bifunctional monomer A
     int moleculesB = 1000; // bifunctional monomer B
-    // moleculeA = type 1, moleculeB = type 2
-
+    // moleculeA = type 0, moleculeB = type 1
+    bool front;
+    bool back;
+    bool something_went_wrong = false;
     struct chain
     {
         std::vector<int> v;
-        int end1;
-        int end2;
+        int frontend; // identity of species at start of vector
+        int backend; // identity of species at end of vector
     };
     // Need to keep track of all the polymer chains that have been created
     typedef std::vector<chain> chain_pool;
-    typedef std::vector<int> test123;
     // also need to keep track of the ends of the chain
     int chainsA = 0; // number of chain ends which are an "A"
     int chainsB = 0; // number of chain ends which are a "B"
@@ -46,64 +47,331 @@ int main() {
     //.. could instead write a loop which goes through chainpool and interrogates 
     // each chain for its ends. However, the above step saves that whole loop
     // as long as properly handled. For difunctional monomers, the first dyad in a chain
-    // will always have end1 = 1 and end2 = 0.
+    // will always have frontend = 0 and backend = 1.
 
     // KMC step
     double kf=A_kf*exp(Ea_kf/(R*T)); // adding a to b
     //double kba=A_kab*exp() 
-    double c = kf/V;
+    //double c = kf/V;
     // ideally rewrite this such that difference in reactivity between A adding to B and B adding to A is taken into account
-    double total_rate =c*(2*moleculesA+chainsA)*(2*moleculesB+chainsB); // 2*molecules because each monomer is bifunctional
+    //double total_rate =c*(2*moleculesA+chainsA)*(2*moleculesB+chainsB); // 2*molecules because each monomer is bifunctional
     
     // I should rewrite this in terms of reaction parameter c rather than k
     std::srand(std::time(0));
-    double r1 = 1.0*std::rand()/RAND_MAX; 
-    double tau = (1/total_rate)*log(1/r1); // calculate tau
-
-    // pick which reaction to have occur
-    double r2 = std::rand()/RAND_MAX;
-    double reaction = r2*total_rate;
-
-    // if (1==1) //(reaction < moleculesB)
-    //     // react B
-    //     moleculesB-=1;
-
-        
-        // update chain end
-    // pick which chain to react...
-    // double r3 = std::rand()/RAND_MAX;
 
     double time = 0;
-    double simulation_time = tau*1000;
+    double simulation_time = 1.0E-5;
     chain_pool all_chains;
+    chain_pool loops;
 
     // KMC loop
     while (time<simulation_time) {
         // first, case of constant concentrations
-
+        front=false;
+        back=false;
         // calculate propensity functions
-
+        double c = kf/V;
         // update total rate
+        double total_rate =c*(2*moleculesA+chainsA)*(2*moleculesB+chainsB); // 2*molecules because each monomer is bifunctional
 
         // choose timestep tau
         double r1 = 1.0*std::rand()/RAND_MAX; 
         double tau = (1/total_rate)*log(1/r1); // calculate timestep tau
 
         // choose reaction to take place
-        // initial case: all reactions are equally likely
-        //if (1==1) {
+        // pick which reaction to have occur
+        // right now all reactions are equally likely
+        double r2 = 1.0*std::rand()/RAND_MAX;
+        double reaction = r2*total_rate;
+
+        // pick which A, B
+        double r3 = 1.0*std::rand()/RAND_MAX;
+        double whichA = r3*(chainsA+2*moleculesA);
+        double r4 = 1.0*std::rand()/RAND_MAX;
+        double whichB = r4*(chainsB+2*moleculesB); 
+        
         // case for which two monomers react to form new chain
-        chain newchain;
-        newchain.end1 = 1;
-        newchain.end2 = 2;
-        newchain.v.push_back(newchain.end1);
-        newchain.v.push_back(newchain.end2);
-        all_chains.push_back(newchain);
-        moleculesA-=1;
-        moleculesB-=1;
-        //}
+        if (whichA<2*moleculesA && whichB<2*moleculesB) {
+            chain newchain;
+            newchain.frontend = 0; // i.e. front end is A
+            newchain.backend = 1; // backend is defined arbitrarily as species B on new polymer chain
+            newchain.v.push_back(newchain.frontend);
+            newchain.v.push_back(newchain.backend);
+            all_chains.push_back(newchain);
+            moleculesA--;
+            moleculesB--;
+            chainsA++;
+            chainsB++;  
+        }
+
+        // case for which one A monomer reacts with one B chain
+        if (whichA<2*moleculesA && whichB>2*moleculesB) {
+            // STEP 1: Select B chain
+            int Bselect = (int) (whichB-2*moleculesB);
+            int selected_chain = 0;
+            int countB = 0;
+
+            while (selected_chain<all_chains.size()){
+                // select which B chain and which B end
+                if (all_chains[selected_chain].backend==1){
+                    countB++;
+                }
+                if (Bselect<countB) {
+                    back=true;
+                    break;
+                }
+                if (all_chains[selected_chain].frontend==1){
+                    countB++;
+                }
+                if (Bselect<countB) {
+                    front=true;
+                    break;
+                }
+                selected_chain++;
+            }
+
+            // STEP 2: which end of the chain has B?
+            // CASE 1: if the B is at the end of the chain
+            if (1 == all_chains[selected_chain].v.back() && back) {
+                // then add A to the end
+                all_chains[selected_chain].v.push_back(0); // can I store a pointer to all_chains[selected_chain] as something shorter?
+                // and update the trackers accordingly
+                all_chains[selected_chain].backend=0;
+                chainsB--;
+                chainsA++;
+                moleculesA--;
+            }
+            // CASE 2: if the B is at the front of the chain
+            else if (1 == all_chains[selected_chain].v.front() && front) {
+                // then add A to the front
+                all_chains[selected_chain].v.insert(all_chains[selected_chain].v.begin(),0);
+                // and update the trackers accordingly
+                all_chains[selected_chain].frontend=0;
+                chainsB--;
+                chainsA++;
+                moleculesA--;
+            }
+            // could add a line to check that this is occuring properly
+        }
+        // case for which 1 B monomer reacts with 1 A chain
+        if (whichA>2*moleculesA && whichB<2*moleculesB) {
+            // STEP 1: Select A chain
+            int Aselect = (int) (whichA-2*moleculesA);
+            int selected_chain = 0;
+            int countA = 0;
+            while (selected_chain<all_chains.size()) {
+                // select which A chain and which A end
+                if (all_chains[selected_chain].backend==0){
+                    countA++;
+                }
+                if (Aselect<countA) {
+                    back=true;
+                    break;
+                }
+                if (all_chains[selected_chain].frontend==0){
+                    countA++;
+                }
+                if (Aselect<countA) {
+                    front=true;
+                    break;
+                }
+                selected_chain++;
+            }
+
+            // STEP 2: which end of the chain has A?
+            // CASE 1: if the A is at the end of the chain .. but what to do if both ends have A?
+            if (0 == all_chains[selected_chain].v.back() && back) {
+                // then add B to the end
+                all_chains[selected_chain].v.push_back(1); // can I store a pointer to all_chains[selected_chain] as something shorter?
+                // and update the trackers accordingly
+                all_chains[selected_chain].backend=1;
+                chainsB++;
+                chainsA--;
+                moleculesB--;
+            }
+            // CASE 2: if the A is at the front of the chain
+            else if (0 == all_chains[selected_chain].v.front() && front) {
+                // then add B to the front
+                all_chains[selected_chain].v.insert(all_chains[selected_chain].v.begin(),1);
+                // and update the trackers accordingly
+                all_chains[selected_chain].frontend=1;
+                chainsB++;
+                chainsA--;
+                moleculesB--;
+            }
+            // could add a line to check that this is occuring properly
+        }
+        // case for which 1 A chain reacts with 1 B chain
+        if (whichA>2*moleculesA && whichB>2*moleculesB) {
+            // STEP 1: select A chain
+            int Aselect = (int) (whichA-2*moleculesA);
+            int selected_A_chain = 0;
+            int countA = 0;
+            bool frontA=false;
+            bool backA=false;
+
+            while (selected_A_chain<all_chains.size()) {
+                // select which A chain and which A end
+                if (all_chains[selected_A_chain].backend==0){
+                    countA++;
+                }
+                if (Aselect<countA) {
+                    backA=true;
+                    break;
+                }
+                if (all_chains[selected_A_chain].frontend==0){
+                    countA++;
+                }
+                if (Aselect<countA) {
+                    frontA=true;
+                    break;
+                }
+                selected_A_chain++;
+            }
+            // STEP 2: select B chain
+            int Bselect = (int) (whichB-2*moleculesB);
+            int selected_B_chain = 0;
+            int countB = 0;
+            bool backB=false;
+            bool frontB=false;
+            while (selected_B_chain<all_chains.size()){
+                // select which B chain and which B end
+                if (all_chains[selected_B_chain].backend==1){
+                    countB++;
+                }
+                if (Bselect<countB) {
+                    backB=true;
+                    break;
+                }
+                if (all_chains[selected_B_chain].frontend==1){
+                    countB++;
+                }
+                if (Bselect<countB) {
+                    frontB=true;
+                    break;
+                }
+                selected_B_chain++;
+            }
+            // which chain is getting added to and which chain is getting deleted?
+            bool add_to_chain_A = false;
+            if (selected_A_chain<selected_B_chain) {
+                add_to_chain_A = true;
+            }
+            // case 0: loop formation
+            if (selected_A_chain==selected_B_chain){
+                // delete from vector of chains
+                // add to loops vector. This does not currently consider whether sterically it is possible for this to occur (i.e. loops consisting of 2 monomers are permitted)
+                loops.push_back(all_chains[selected_A_chain]);
+                all_chains.erase(all_chains.begin()+selected_A_chain); // what the heck is an iterator, and what's the difference between it and a const_iterator
+            }
+            // case 1: front of A chain to front of B chain
+            else if (frontA && frontB) {
+                if (add_to_chain_A) {
+                    // reverse A chain, append B chain to end of A chain, update the ends, delete B chain
+                    std::reverse(all_chains[selected_A_chain].v.begin(),all_chains[selected_A_chain].v.end());
+                    all_chains[selected_A_chain].v.insert(all_chains[selected_A_chain].v.end(),all_chains[selected_B_chain].v.begin(),all_chains[selected_B_chain].v.end());
+                    all_chains[selected_A_chain].frontend=all_chains[selected_A_chain].backend;
+                    all_chains[selected_A_chain].backend=all_chains[selected_B_chain].backend;
+                    all_chains.erase(all_chains.begin()+selected_B_chain);
+                }
+                else {
+                    // reverse B chain, append A to end of B chain, update the ends, delete A chain
+                    std::reverse(all_chains[selected_B_chain].v.begin(),all_chains[selected_B_chain].v.end()); 
+                    all_chains[selected_B_chain].v.insert(all_chains[selected_B_chain].v.end(),all_chains[selected_A_chain].v.begin(),all_chains[selected_A_chain].v.end());
+                    all_chains[selected_B_chain].frontend=all_chains[selected_B_chain].backend;
+                    all_chains[selected_B_chain].backend=all_chains[selected_A_chain].backend;
+                    all_chains.erase(all_chains.begin()+selected_A_chain);
+                }
+            }
+            // case 2: front of A chain to back of B chain
+            else if (frontA && backB) {
+                if (add_to_chain_A) {
+                    // add A chain to B chain, then put it in the A chain location, then delete chain B
+                    all_chains[selected_B_chain].v.insert(all_chains[selected_B_chain].v.end(),all_chains[selected_A_chain].v.begin(),all_chains[selected_A_chain].v.end());
+                    all_chains[selected_B_chain].backend=all_chains[selected_A_chain].backend;
+                    std::iter_swap(all_chains.begin()+selected_A_chain,all_chains.begin()+selected_B_chain); 
+                    // I'm not sure that the above and below lines are doing what I am intending
+                    all_chains.erase(all_chains.begin()+selected_B_chain);
+                }
+                else {
+                    // add A chain to back of B chain, delete A chain
+                    all_chains[selected_B_chain].v.insert(all_chains[selected_B_chain].v.end(),all_chains[selected_A_chain].v.begin(),all_chains[selected_A_chain].v.end());
+                    all_chains[selected_B_chain].backend=all_chains[selected_A_chain].backend;
+                    all_chains.erase(all_chains.begin()+selected_B_chain);
+                }
+            }
+            // case 3: back of A chain to front of B chain
+            else if (backA && frontB) {
+                if (add_to_chain_A) {
+                    // add B chain to back of A chain
+                    all_chains[selected_A_chain].v.insert(all_chains[selected_A_chain].v.end(),all_chains[selected_B_chain].v.begin(),all_chains[selected_B_chain].v.end());
+                    all_chains[selected_A_chain].backend=all_chains[selected_B_chain].backend;
+                    all_chains.erase(all_chains.begin()+selected_B_chain);
+                }
+                else {
+                    // add B chain to back of A chain, then put it in the B chain location, then
+                    all_chains[selected_A_chain].v.insert(all_chains[selected_A_chain].v.end(),all_chains[selected_B_chain].v.begin(),all_chains[selected_B_chain].v.end());
+                    all_chains[selected_A_chain].backend=all_chains[selected_B_chain].backend;
+                    //all_chains.at(selected_B_chain)=all_chains[selected_A_chain]; 
+                    std::iter_swap(all_chains.begin()+selected_A_chain,all_chains.begin()+selected_B_chain); 
+                    // I'm not sure that the above and below lines are doing what I am intending
+                    all_chains.erase(all_chains.begin()+selected_A_chain);
+                }
+            }
+            // case 4: back of A chain to back of B chain
+            else if (backA && backB) {
+                if (add_to_chain_A) {
+                    // reverse chain B, add to end of chain A, delete chain B
+                    std::reverse(all_chains[selected_B_chain].v.begin(),all_chains[selected_B_chain].v.end());
+                    all_chains[selected_A_chain].v.insert(all_chains[selected_A_chain].v.end(),all_chains[selected_B_chain].v.begin(),all_chains[selected_B_chain].v.end());
+                    all_chains[selected_A_chain].backend=all_chains[selected_B_chain].frontend;
+                    all_chains.erase(all_chains.begin()+selected_B_chain);
+                }
+                else {
+                    // reverse chain A, add to end of chain B, delete chain A
+                    std::reverse(all_chains[selected_A_chain].v.begin(),all_chains[selected_A_chain].v.end());
+                    all_chains[selected_B_chain].v.insert(all_chains[selected_B_chain].v.end(),all_chains[selected_A_chain].v.begin(),all_chains[selected_A_chain].v.end());
+                    all_chains[selected_B_chain].backend=all_chains[selected_A_chain].frontend;
+                    all_chains.erase(all_chains.begin()+selected_A_chain);
+                }
+            }
+            else
+            {
+                // count up actual number of B chain ends
+                int i = 0;
+                int hey;
+                int countB_verify=0;
+                while (i<all_chains.size()) {
+                    if (all_chains[i].v[0]==1) {
+                        countB_verify++;
+                    }
+                    if (all_chains[i].v.back()==1) {
+                        countB_verify++;
+                    }
+                    hey = all_chains[i].v.back();
+                    i++;
+                }
+                // count up actual number of A chain ends
+                int j = 0;
+                int countA_verify=0;
+                while (j<all_chains.size()) {
+                    if (all_chains[j].v[0]==0) {
+                        countA_verify++;
+                    }
+                    if (all_chains[j].v.back()==0) {
+                        countA_verify++;
+                    }
+                    j++;
+                }
+
+                something_went_wrong = true;
+                
+            }
+            chainsA--;
+            chainsB--;
+        }
         time += tau;
     }
-    // total number of events that can occur are 1 for each reaction 
+    // total number of events that can occur is 1 for each reaction 
     return 0;
 }
--- playground.cpp
+++ playground.cpp
@@ -2,16 +2,50 @@
 #include<stdlib.h>
 #include<iostream>
 #include<string>
-#include<vector>
+#include <vector>
 #include<math.h>
 
 // author: Matthew
 // playground file for understanding C++
+//template class std::vector<MyClass>;
 
 int main() {
     std::srand(std::time(NULL));
     double r1 = 1.0*std::rand()/RAND_MAX; 
     double r2 = 1.0*std::rand()/RAND_MAX;
     int foo = RAND_MAX;
+    std::vector<int> test123 {1,2,3,4,5,6};
+
+    // At some point, make sure I know where to find the output from printf... debug folder?
+    printf("the 2nd value in the array test123 is %i", test123[1]);
+    //double x=test123[1];
+    std::reverse(test123.begin(),test123.end());
+    std::iter_swap(test123.begin()+1,test123.begin()+4);
+    test123.erase(test123.begin()+4);
+    struct chain
+    {
+        std::vector<int> v;
+        int frontend; // identity of species at start of vector
+        int backend; // identity of species at end of vector
+    };
+    typedef std::vector<chain> chain_pool;
+    chain_pool all_chains;
+    chain chain1;
+    chain1.frontend=1;
+    chain1.backend=0;
+    chain1.v = {0,0,1,6};
+    all_chains.push_back(chain1);
+    all_chains.push_back(chain1);
+    all_chains.push_back(chain1);
+    all_chains.push_back(chain1);
+    all_chains.erase(all_chains.begin()+1);
+
+    bool bar = (all_chains[0].v[0]==1);
+    bool barend = (all_chains[0].v[all_chains[0].v.back()]==0);
+    bool hmmm = (all_chains[0].v.back()==3);
+    int sizer = all_chains[0].v.size(); // this is 4
+    int lol = all_chains[0].v.front(); // I understand why this is 0
+    int wtf = all_chains[0].v.back(); // but why is this also 0?
     return 0;
+
 }
